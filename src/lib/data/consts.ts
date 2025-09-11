14563e644c35cc75adec022645ceaa8770caa163
Discokatten <isabelle.wincrantz@gmail.com>
2025-09-11 15:20:45 +0200

    added lib folder with interfaces

--- src/lib/data/consts.ts
+++
@@ -1,7 +0,0 @@
-export const MENUITEMS = [
-  { title: 'Home', path: '/' },
-  { title: 'Men', path: '/products?category=men' },
-  { title: 'Women', path: '/products?category=women'},
-  { title: 'About', path: '/about' },
-  { title: 'Contact', path: '/contact' },
-];

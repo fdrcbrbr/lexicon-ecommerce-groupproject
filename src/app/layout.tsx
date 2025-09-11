8f4558449d16e4817d48548b21a77aa3492a0315
Lorenzo <lorenzo.dastoli@gmail.com>
2025-09-11 14:35:49 +0200

    update package.json and font in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
-// Import Inter da Google Fonts
 const inter = Inter({
-  variable: "--font-inter",
+  variable: "--font-inter", 
   subsets: ["latin"],
   display: "swap",
 });
-
 
 export const metadata: Metadata = {
   title: "Shop.co",

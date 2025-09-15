4fa26af4db1beb62b2e2dc0a723928b04854db3c
Alexander Stauch <blacke4dawn@gmail.com>
2025-09-15 11:43:17 +0200

    product details query & interface

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,4 +1,4 @@
-import { ProductsRes } from "@/lib/interfaces";
+import { ProductsRes, ProductDetails } from "@/lib/interfaces";
 const baseurl = `https://dummyjson.com/`;
 
 export async function getProductsByCategory(category?: string) {
@@ -13,8 +13,25 @@ export async function getProductsByCategory(category?: string) {
   } catch (error) {
     console.error("Error fetching characters:", error);
     throw new Error(
-      `Failed to fetch characters: ${
-        error instanceof Error ? error.message : String(error)
+      `Failed to fetch characters: ${error instanceof Error ? error.message : String(error)
+      }`
+    );
+  }
+}
+
+export async function getProductDetails(id: string) {
+  try {
+    const response = await fetch(`${baseurl}products/${id}`);
+    if (!response.ok) {
+      throw new Error("bad response");
+    }
+    const data: ProductDetails = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    throw new Error(
+      `Failed to fetch product details: ${error instanceof Error ? error.message : String(error)
       }`
     );
   }

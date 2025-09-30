b6e38fb42b95c19b265978ae829b457a7866c061
Alexander Stauch <blacke4dawn@gmail.com>
2025-09-30 11:26:29 +0200

    server action: update product

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -38,10 +38,44 @@ export async function createProduct(prevState: ActionState | null, formData: For
     const transformedProduct = { ...data, category: groupedCategory };
 
     return { success: true, product: transformedProduct };
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    return { success: false, error: "An unexpected error occurred" };
+    return { success: false, error: `An unexpected error occurred: ${error}` };
   }
 }
 
+export async function updateProduct(prevState: ActionState | null, formData: FormData) {
+  try {
+    const id = formData.get("id") as string;
+    const title = formData.get("title") as string;
+    const category = formData.get("category") as string;
+    const stock = formData.get("stock") as string;
+    const price = formData.get("price") as string;
+    const discountPercentage = formData.get("discountPercentage") as string;
 
+    const updatedProduct = {
+      title,
+      price: parseInt(price),
+      category,
+      stock: parseInt(stock),
+      discountPercentage: parseInt(discountPercentage),
+    };
+
+    const res = await fetch(`https://dummyjson.com/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedProduct),
+    });
+
+    if (!res.ok) {
+      return { success: false, error: "Failed to update product" };
+    }
+
+    const data = await res.json();
+    const groupedCategory = mapSpecificToGroupedCategory(category);
+    const transformedProduct = { ...data, category: groupedCategory };
+
+    return { success: true, product: transformedProduct };
+  } catch (error) {
+    return { success: false, error: `An unexpected error occurred: ${error}` };
+  }
+}
